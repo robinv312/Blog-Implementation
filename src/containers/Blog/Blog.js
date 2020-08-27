b804de5442b3bf9f3eacb0b14b8daecdf6e1e11c
Robin Varghese <robinv312@gmail.com>
2020-08-27 18:44:59 +0530

    routing updation commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,68 +1,38 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
 import './Blog.css';
-
+import {Route,NavLink} from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
+  render() {
+   return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/" exact
+                            activeClassName="my-active"
+                            activeStyle={{
+                                color:'#fa923f',
+                                textDecoration: 'underline'
+                            }}
+                            >Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname:'/new-post',
+                                hash: '#submit',
+                                search:'?quick-submit=true'
 
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                // console.log(error);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
-        return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+               {/* <Route path="/" exact render={()=><h1>Home</h1>}/>
+               <Route path="/" exact render={()=><h1>Home2</h1>}/> */}
+               <Route path="/" exact component={Posts}/>
+               <Route path="/new-post"  component={NewPost}/>
             </div>
+            
         );
     }
 }
-
 export default Blog;
